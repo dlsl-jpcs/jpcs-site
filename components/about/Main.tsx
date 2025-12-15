92ef9f53c16da6636778a6469554529103fc1489
Mikaela Shane Estrellanes <mikaela_shane_estrellanes@dlsl.edu.ph>
2025-12-15 21:38:18 +0800

    fix programs under and add about

---
+++ components/about/Main.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Main = () => {
+  return (
+    <section className="py-24 px-6 text-accent-foreground relative overflow-hidden">
+      <div className="absolute -top-1/4 -right-1/4 w-[800px] h-[800px] bg-accent-green/5 rotate-45 transform"></div>
+      {/* content */}
+      <div className="relative z-10 w-full md:max-w-6xl ">
+      </div>
+    </section>
+  );
+};
+
+export default Main;
